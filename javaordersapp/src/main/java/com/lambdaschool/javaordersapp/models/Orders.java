058b31e23ff6fcd8971b67c6cff233fe6b69b821
Christopher Corvo <christophercorvo@gmail.com>
2020-12-07 19:24:28 -0500

    added Orders and payments packages

--- javaordersapp/src/main/java/com/lambdaschool/javaordersapp/models/Orders.java
+++ javaordersapp/src/main/java/com/lambdaschool/javaordersapp/models/Orders.java
@@ -1,5 +1,77 @@
 package com.lambdaschool.javaordersapp.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "orders")
 public class Orders
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long ordnum;
+
+    private double ordamount;
+    private double advanceamount;
+
+    //custcode Long foreign key (one customer to many order not null
+
+    private String orderdescription;
+
+    public Orders()
+    {
+      // default constructor for JPA
+    }
+    //constructor with parameters
+    public Orders(
+        double ordamount,
+        double advanceamount,
+        String orderdescription)
+    {
+        this.ordnum = ordnum;
+        this.ordamount = ordamount;
+        this.advanceamount = advanceamount;
+        this.orderdescription = orderdescription;
+    }
+
+    // ----- Getters and Setters ------
+
+    public long getOrdnum()
+    {
+        return ordnum;
+    }
+
+    public void setOrdnum(long ordnum)
+    {
+        this.ordnum = ordnum;
+    }
+
+    public double getOrdamount()
+    {
+        return ordamount;
+    }
+
+    public void setOrdamount(double ordamount)
+    {
+        this.ordamount = ordamount;
+    }
+
+    public double getAdvanceamount()
+    {
+        return advanceamount;
+    }
+
+    public void setAdvanceamount(double advanceamount)
+    {
+        this.advanceamount = advanceamount;
+    }
+
+    public String getOrderdescription()
+    {
+        return orderdescription;
+    }
+
+    public void setOrderdescription(String orderdescription)
+    {
+        this.orderdescription = orderdescription;
+    }
 }
--- javaordersapp/src/main/java/com/lambdaschool/javaordersapp/models/Payments.java
+++ javaordersapp/src/main/java/com/lambdaschool/javaordersapp/models/Payments.java
@@ -1,5 +1,38 @@
 package com.lambdaschool.javaordersapp.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "payments")
 public class Payments
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long paymentid;
+
+    private String type;
+
+    public Payments()
+    {
+        // Default constructor for JPA
+    }
+
+    // Constructor with parameters
+
+    public Payments(String type)
+    {
+        this.type = type;
+    }
+
+    // ------- Getter and Setter -----
+
+    public String getType()
+    {
+        return type;
+    }
+
+    public void setType(String type)
+    {
+        this.type = type;
+    }
 }
